c67551074afb020549950dcaea2a53069a14e770
liwei <121088825@qq.com>
2016-09-09 17:59:43 +0800

    更新了 Lucene 索引类的后台代码。

--- src/main/java/com/liwei/lucene/BlogIndex.java
+++ src/main/java/com/liwei/lucene/BlogIndex.java
@@ -97,6 +97,7 @@ public class BlogIndex {
         // 添加索引的时候，博客的内容没有必要把标签信息作为索引的内容
         document.add(new TextField("content",blog.getContextNoTag(), Field.Store.YES));
         try {
+            // 使用 IndexWriter 添加索引
             indexWriter.addDocument(document);
             indexWriter.close();
         } catch (IOException e) {
@@ -104,12 +105,20 @@ public class BlogIndex {
         }
     }
 
+
+    /**
+     * 删除指定博客的索引（和更新指定博客的索引一样，通过 Term 对象，指定 id 进行删除）
+     * @param blogId
+     */
     public void deleteIndex(String blogId){
         IndexWriter indexWriter = getWriter();
         try {
             indexWriter.deleteDocuments(new Term("id",blogId));
-            indexWriter.forceMergeDeletes();// 强制删除
+            // 强制删除
+            indexWriter.forceMergeDeletes();
+            // 索引提交（只有删除的时候有索引的提交，添加和更新的时候并不调用 commit() 方法）
             indexWriter.commit();
+            // 索引关闭
             indexWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -117,6 +126,14 @@ public class BlogIndex {
 
     }
 
+
+    /**
+     * 更新博客索引（更新索引的内容其实和添加索引是一样的）
+     *
+     * 通过 Term （id） 这个条件进行索引的更新
+     *
+     * @param blog
+     */
     public void updateIndex(Blog blog){
         IndexWriter indexWriter = getWriter();
         Document document = new Document();
@@ -124,21 +141,32 @@ public class BlogIndex {
         document.add(new TextField("title",String.valueOf(blog.getTitle()), Field.Store.YES));
         String dateStr = sdf.format(blog.getReleaseDate());
         document.add(new StringField("releaseDate",dateStr, Field.Store.YES));
+        document.add(new TextField("content",blog.getContextNoTag(), Field.Store.YES));
         try {
+            // 使用 IndexWriter 更新索引
             indexWriter.updateDocument(new Term("id",String.valueOf(blog.getId())),document);
             indexWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+
+    /**
+     * 【重要方法】
+     * 根据关键字查询索引
+     * @param q
+     * @return
+     */
     public List<Blog> searchBlog(String q){
         try {
             directory = FSDirectory.open(Paths.get(indexDir));
             IndexReader indexReader = DirectoryReader.open(directory);
             IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
+            // 同时搜索“标题”和“内容”中含有 搜索关键字的内容
+            // 所以我们使用 BooleanQuery
+            // 中文分词器只要用一个 ，但是 QueryParser 要用 2 个
             BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
             SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
             QueryParser parser1 = new QueryParser("title",analyzer);
@@ -150,25 +178,40 @@ public class BlogIndex {
             booleanQuery.add(query1, BooleanClause.Occur.SHOULD);
             booleanQuery.add(query2, BooleanClause.Occur.SHOULD);
 
+            // 查找前 100 条符合条件的记录
             TopDocs hits = indexSearcher.search(booleanQuery.build(),100);
             QueryScorer scorer = new QueryScorer(query1);
 
             // 高亮
+            // 通过得分包装一个 Fragmenter 片段对象
             Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
+            // 将搜索关键字进行高亮格式设置（前后加上 HTML 的标签）
             SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<b><font color='red'>","</font></b>");
+            // 根据 格式对象 和 得分对象 创建高亮对象
             Highlighter highlighter = new Highlighter(simpleHTMLFormatter,scorer);
+            // 设置高亮对象的文本片段对象
             highlighter.setTextFragmenter(fragmenter);
 
+
+            // 封装搜索结果返回的对象（就使用 Blog 的 List 对象 最合适）
             List<Blog> blogList = new ArrayList<>();
             Document doc = null;
             Blog blog = null;
             for(ScoreDoc scoreDoc:hits.scoreDocs){
+                // 从 Document 对象中获得关于 Blog 对象的相关信息
                 doc = indexSearcher.doc(scoreDoc.doc);
                 blog = new Blog();
                 blog.setId(Integer.parseInt(doc.get("id")));
                 blog.setReleaseDateStr(doc.get("releaseData"));
                 String title = doc.get("title");
+
+                // 为了使得一些特殊符合能够正常显示
+                // 参考资料：利用StringEscapeUtils对字符串进行各种转义与反转义（Java）
+                // http://blog.csdn.net/chenleixing/article/details/43456987
                 String content = StringEscapeUtils.escapeXml(doc.get("content"));
+
+                // title 和 content 都是我们搜索的关键字的范围，所以下面这两个代码片段很关键
+                // 目的是为了获得含有搜索关键字的最佳片段
 
                 // 拿到最佳片段
                 if(title!=null){
@@ -181,6 +224,7 @@ public class BlogIndex {
                     }
                 }
 
+                // 拿到最佳片段
                 if(content!=null){
                     TokenStream tokenStream = analyzer.tokenStream("content",new StringReader(content));
                     String hContent = highlighter.getBestFragment(tokenStream,content);
